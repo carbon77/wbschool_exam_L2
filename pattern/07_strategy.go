926458f91879b12093ed1648cb5b9c0b199c8ff8
Igor <zakatov0303@gmail.com>
2024-12-22 22:28:40 +0300

    Add patterns: 7 strategy, 8 state

--- pattern/07_strategy.go
+++ pattern/07_strategy.go
@@ -1,7 +1,78 @@
 package pattern
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 	Реализовать паттерн «стратегия».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Strategy_pattern
 */
+
+type RouteStrategy interface {
+	BuildRoute(A, B string, kilometers float64) (time time.Duration, price float64)
+}
+
+type CarStrategy struct {
+	AvgCarSpeed    float64
+	AvgPetrolPrice float64
+}
+
+func (cs *CarStrategy) BuildRoute(A, B string, kilometers float64) (time.Duration, float64) {
+	hours := kilometers / cs.AvgCarSpeed
+	return time.Duration(hours * float64(time.Hour)), cs.AvgPetrolPrice * hours
+}
+
+type WalkingStrategy struct {
+	AvgWalkingSpeed float64
+}
+
+func (ws *WalkingStrategy) BuildRoute(A, B string, kilometers float64) (time.Duration, float64) {
+	hours := kilometers / ws.AvgWalkingSpeed
+	return time.Duration(hours * float64(time.Hour)), 0
+}
+
+type PublicTransportStrategy struct {
+	AvgTrainSpeed float64
+	AvgTrainPrice float64
+}
+
+func (pts *PublicTransportStrategy) BuildRoute(A, B string, kilometers float64) (time.Duration, float64) {
+	hours := kilometers / pts.AvgTrainSpeed
+	return time.Duration(hours * float64(time.Hour)), pts.AvgTrainPrice * hours
+}
+
+type Navigator struct {
+	Strategies []RouteStrategy
+}
+
+func (n *Navigator) BuildRoute(A, B string, kilometers float64) {
+	fmt.Printf("Путь между %s и %s:\n", A, B)
+	for _, routeStrategy := range n.Strategies {
+		travelTime, price := routeStrategy.BuildRoute(A, B, kilometers)
+
+		fmt.Print("\t")
+		switch routeStrategy.(type) {
+		case *CarStrategy:
+			fmt.Print("На машине: ")
+		case *WalkingStrategy:
+			fmt.Print("Пешком: ")
+		case *PublicTransportStrategy:
+			fmt.Print("На общ. транспорте: ")
+		}
+		fmt.Printf("время = %.2f час, цена = %.2f руб\n", travelTime.Hours(), price)
+	}
+}
+
+func TestStrategy() {
+	carStrategy := &CarStrategy{100, 400}
+	walkingStrategy := &WalkingStrategy{4}
+	publicTransportStrategy := &PublicTransportStrategy{60, 200}
+	navigator := &Navigator{
+		Strategies: []RouteStrategy{carStrategy, walkingStrategy, publicTransportStrategy},
+	}
+	navigator.BuildRoute("Москва", "Зеленоград", 42)
+	navigator.BuildRoute("Казань", "Москва", 720)
+}
--- pattern/08_state.go
+++ pattern/08_state.go
@@ -1,7 +1,54 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «состояние».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/State_pattern
 */
+
+type MobileState int
+
+const (
+	POWER_OFF MobileState = iota
+	SCREEN_DISABLED
+	SCREEN_ON
+)
+
+type MobileScreen struct {
+	state MobileState
+}
+
+func (ms *MobileScreen) PowerOn() {
+	ms.state = SCREEN_DISABLED
+}
+
+func (ms *MobileScreen) Touch() {
+	if ms.state == POWER_OFF {
+		return
+	}
+
+	if ms.state == SCREEN_DISABLED {
+		ms.state = SCREEN_ON
+		return
+	}
+
+	fmt.Println("Touch!")
+}
+
+func (ms *MobileScreen) Swipe() {
+	if ms.state != SCREEN_ON {
+		return
+	}
+
+	fmt.Println("Swipe!")
+}
+
+func TestState() {
+	mobileScreen := &MobileScreen{}
+	mobileScreen.Touch()
+	mobileScreen.PowerOn()
+	mobileScreen.Touch()
+	mobileScreen.Touch()
+}
